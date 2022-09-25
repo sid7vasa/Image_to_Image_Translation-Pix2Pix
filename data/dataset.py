e45850e2a1de306545adfb44fb982da584a0a270
Santosh Vasa <santosh7vasa@outlook.com>
2022-09-24 18:00:18 -0700

    Finished TFRecords storing and loading. [Verified]

--- data/dataset.py
+++ data/dataset.py
@@ -9,59 +9,121 @@ import numpy as np
 from tensorflow.keras.preprocessing.image import load_img, img_to_array
 from imutils import paths
 import yaml
+import os
+from PIL import Image
 
 
 def _bytes_feature(value):
-    if isinstance(value, type(tf.constant(0))): # if value ist tensor
-        value = value.numpy() # get value of tensor
+    if isinstance(value, type(tf.constant(0))):  # if value ist tensor
+        value = value.numpy()  # get value of tensor
     return tf.train.Feature(bytes_list=tf.train.BytesList(value=[value]))
 
+
 def _float_feature(value):
-  return tf.train.Feature(float_list=tf.train.FloatList(value=[value]))
+    return tf.train.Feature(float_list=tf.train.FloatList(value=[value]))
+
 
 def _int64_feature(value):
-  return tf.train.Feature(int64_list=tf.train.Int64List(value=[value]))
+    return tf.train.Feature(int64_list=tf.train.Int64List(value=[value]))
+
 
 def serialize_array(array):
-  array = tf.io.serialize_tensor(array)
-  return array
+    array = tf.io.serialize_tensor(array)
+    return array
+
 
 def parse_single_image(image, label):
-  data = {
-        'height' : _int64_feature(image.shape[0]),
-        'width' : _int64_feature(image.shape[1]),
-        'depth' : _int64_feature(image.shape[2]),
-        'raw_image' : _bytes_feature(serialize_array(image)),
-        # 'label' : _int64_feature(label),
-        'l_height' : _int64_feature(label.shape[0]),
-        'l_width' : _int64_feature(label.shape[1]),
-        'l_depth' : _int64_feature(label.shape[2]),
-        'l_raw_image' : _bytes_feature(serialize_array(label)),
+    data = {
+        'height': _int64_feature(image.shape[0]),
+        'width': _int64_feature(image.shape[1]),
+        'depth': _int64_feature(image.shape[2]),
+        'raw_image': _bytes_feature(serialize_array(image)),
+        'l_height': _int64_feature(label.shape[0]),
+        'l_width': _int64_feature(label.shape[1]),
+        'l_depth': _int64_feature(label.shape[2]),
+        'l_raw_image': _bytes_feature(serialize_array(label)),
     }
 
-  out = tf.train.Example(features=tf.train.Features(feature=data))
-  return out
+    out = tf.train.Example(features=tf.train.Features(feature=data))
+    return out
 
-def write_images_to_tfr_short(images, labels, filename:str="train"):
-  filename= filename+".tfrecords"
-  writer = tf.io.TFRecordWriter(filename) 
-  count = 0
 
-  for index in range(len(images)):
-    current_image = images[index] 
-    current_label = labels[index]
+def parse_tfr_element(element):
+    # use the same structure as above; it's kinda an outline of the structure we now want to create
+    data = {
+        'height': tf.io.FixedLenFeature([], tf.int64),
+        'width': tf.io.FixedLenFeature([], tf.int64),
+        'raw_image': tf.io.FixedLenFeature([], tf.string),
+        'depth': tf.io.FixedLenFeature([], tf.int64),
+        'l_height': tf.io.FixedLenFeature([], tf.int64),
+        'l_width': tf.io.FixedLenFeature([], tf.int64),
+        'l_raw_image': tf.io.FixedLenFeature([], tf.string),
+        'l_depth': tf.io.FixedLenFeature([], tf.int64),
+    }
 
-    out = parse_single_image(image=current_image, label=current_label)
-    writer.write(out.SerializeToString())
-    count += 1
+    content = tf.io.parse_single_example(element, data)
 
-  writer.close()
-  print(f"Wrote {count} elements to TFRecord")
-  return count
+    height = content['height']
+    width = content['width']
+    depth = content['depth']
+    raw_image = content['raw_image']
+    l_height = content['l_height']
+    l_width = content['l_width']
+    l_depth = content['l_depth']
+    l_raw_image = content['l_raw_image']
+
+    # get our 'feature'-- our image -- and reshape it appropriately
+    feature = tf.io.parse_tensor(raw_image, out_type=tf.uint8)
+    feature = tf.reshape(feature, shape=[height, width, depth])
+    label = tf.io.parse_tensor(l_raw_image, out_type=tf.uint8)
+    label = tf.reshape(label, shape=[l_height, l_width, l_depth])
+    return (feature, label)
+
+
+def split_image(image_path):
+    image = Image.open(image_path)
+    image = image.resize((512,256))
+    image = np.asarray(image)
+    image, label = image[:, :256], image[:, 256:]
+    return image, label
+
+
+def write_images_to_tfr_short(store_dir, image_paths, filename: str = "train"):
+    """ 
+    Modify this function to change the dataset input format.
+    """
+    filename = filename+".tfrecords"
+    writer = tf.io.TFRecordWriter(os.path.join(
+        store_dir, filename))
+    count = 0
+
+    for image_path in image_paths:
+        current_image, current_label = split_image(image_path)
+        out = parse_single_image(image=current_image, label=current_label)
+        writer.write(out.SerializeToString())
+        count += 1
+
+    writer.close()
+    print(f"Wrote {count} elements to TFRecord")
+    return count
 
 
 def generate_tfrecords(parameters):
-    image_paths = paths.list_images(parameters['dataset']['data_dir']['train'])
-    
-    count = write_images_to_tfr_short(images_small, labels_small, filename="train")
+    train_paths = paths.list_images(parameters['dataset']['data_dir']['train'])
+    val_paths = paths.list_images(parameters['dataset']['data_dir']['val'])
+    count_train = write_images_to_tfr_short(
+        parameters['dataset']['data_dir']['train'], train_paths, filename="train")
+    count_val = write_images_to_tfr_short(
+        parameters['dataset']['data_dir']['val'], val_paths, filename='val')
+    print("Number of images in Train:", count_train)
+    print("Number of images in Val:", count_val)
     return
+
+
+def load_tfrecords(parameters):
+    train_dataset = tf.data.TFRecordDataset(os.path.join(parameters['dataset']['data_dir']['train'], 'train.tfrecords'))
+    val_dataset = tf.data.TFRecordDataset(os.path.join(parameters['dataset']['data_dir']['val'], 'val.tfrecords'))
+    train_dataset = train_dataset.map(parse_tfr_element)
+    val_dataset = val_dataset.map(parse_tfr_element)
+    return (train_dataset, val_dataset)
+    
--- src/train.py
+++ src/train.py
@@ -4,11 +4,13 @@ Created on Sun Sep 18 22:44:57 2022
 
 @author: santosh
 """
+from data.dataset import generate_tfrecords, load_tfrecords
 from tensorflow.keras.utils import plot_model
 from models.pi2pix import Discriminator, Generator, GAN
 import yaml
 import tensorflow as tf
 import os
+import matplotlib.pyplot as plt
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 tf.get_logger().setLevel('WARNING')
 
@@ -16,9 +18,27 @@ tf.get_logger().setLevel('WARNING')
 print(tf.test.is_gpu_available())
 
 
-def run(parameters):
-    disc = Discriminator((256, 256, 3)).get_model()
-    return disc
+def get_dataset(parameters):
+    if not os.path.exists(os.path.join(
+            parameters['dataset']['data_dir']['train'],
+            'train.tfrecords')) or not os.path.exists(os.path.join(
+                parameters['dataset']['data_dir']['val'], 'val.tfrecords')):
+        print("Generating TF Records:")
+        generate_tfrecords(parameters)
+    else:
+        print("Using existing TF Records")
+    train_dataset, val_dataset = load_tfrecords(parameters)
+    return train_dataset, val_dataset
+
+def visualize_datasets(train_dataset, val_dataset):
+    for data in train_dataset.take(1):
+        print(data[0].shape)
+        print(data[1].shape)
+        plt.imshow(data[1])
+        plt.show()
+
+def train(parameters, generator, discriminator, gan, dataset):
+    pass
 
 
 if __name__ == "__main__":
@@ -33,3 +53,8 @@ if __name__ == "__main__":
     gan = GAN(generator, discriminator, (256, 256, 3)).get_model()
     print(gan.summary())
     plot_model(gan, to_file="gan.png")
+    train_dataset, val_dataset = get_dataset(parameters)
+    print(train_dataset, val_dataset)
+    visualize_datasets(train_dataset, val_dataset)
+    
+    
